759afbe9751460d608952dbe08f63f40e7209c1b
AbduboriyMirzayev <milion_programs@mail.ru>
2021-04-13 11:12:35 +0500

    T3 Done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "antd/dist/antd.css";
 import { useSelector } from "react-redux";
